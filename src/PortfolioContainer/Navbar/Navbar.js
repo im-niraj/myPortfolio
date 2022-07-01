1354d0f4b79759e62c94f92401004c14cffcdee5
Niraj Kumar <n1r4jkumar@gmail.com>
2022-07-01 20:41:22 +0530

    toggal menu added

--- src/PortfolioContainer/Navbar/Navbar.js
+++ src/PortfolioContainer/Navbar/Navbar.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from 'react-scroll';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
-   const abc = (to) => {
-        console.log(to);
-    };
+    const [flag, setFlag] = useState(false);
+   const toggleButton = () => {
+        if(flag){
+            document.getElementById("toggleMenu").style.display = 'block';
+            setFlag(!flag);
+        }
+        else{
+            document.getElementById("toggleMenu").style.display = 'none';
+            setFlag(!flag);
+        }
+   }
 
     window.addEventListener('scroll', () =>{
         let navbar = document.querySelector('.navbar-header');
@@ -41,9 +49,24 @@ export default function Navbar() {
                 
             </div>
 
-            <button className="navbar-toggler" data-toggle="open-navbar1">
+            <button className="navbar-toggler" data-toggle="open-navbar1" onClick={toggleButton}>
             <i class="fa fa-bars" aria-hidden="true"></i>
             </button>
+        </div>
+        <div id="toggleMenu" className="hiddenMenu">
+                <li><Link to="Home" spy={true} smooth={true} offset={0} duration={500} >
+                    Home
+                </Link></li>
+                <li><Link to="About" spy={true} smooth={true} offset={0} duration={500} >
+                    About
+                </Link></li>
+                <li><Link to="Projects" spy={true} smooth={true} offset={0} duration={500}>
+                    Projects
+                </Link></li>
+                <li><Link to="Contact" spy={true} smooth={true} offset={0} duration={500}>
+                    Contact
+                </Link></li>
+
         </div>
     </div>
   );
