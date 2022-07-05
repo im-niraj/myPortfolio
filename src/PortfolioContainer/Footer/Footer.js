c61aff83aed08eb629d14a7f9aecb4fe4d80f112
Niraj Kumar <n1r4jkumar@gmail.com>
2022-07-05 08:15:01 +0530

    footer updated

--- src/PortfolioContainer/Footer/Footer.js
+++ src/PortfolioContainer/Footer/Footer.js
@@ -1,95 +1,40 @@
 import React from 'react'
 import './Footer.css';
-import {Link} from "react-scroll";
+import {ImHeart} from 'react-icons/im';
 export default function Footer() {
+    let date = new Date().getFullYear();
   return (
     <footer className="site-footer">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12 col-md-6">
-          <h6>About</h6>
-          <p className="text-justify">I am a Java Backend developer and also a Frontend developer, I build websites using HTML, CSS and JavaScript. </p>
-        </div>
+        <div className="container-footer">
+                <div className="">
+                    <div className=" footer-toop">
+                        <h3 id='NameFooter'>NIRAJ KUMAR</h3>
+                        <p>I'm a Java Backend Developer. Experienced with all stages of the development cycle for dynamic web projects.</p>
+                        <div className='footer-social'>
+                        <a href="https://github.com/im-niraj/" target="_blank">
+                            <i class="fa fa-github" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/imniraj/" target="_blank">
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.instagram.com/n1r4jkumar/" target="_blank">
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCMj15TEUdhEjr2YHL-qO7uQ" target="_blank">
+                            <i className='fa fa-youtube-play'></i>
+                        </a>
+                        <a href="https://twitter.com/n1r4jkumar" target="_blank">
+                            <i className='fa fa-twitter'></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        <div className="col-xs-6 col-md-3">
-          <h6>Tech Stack</h6>
-          <ul className="footer-links">
-            <li>Java</li>
-            <li>SQL</li>
-            <li>Spring Boot</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-          </ul>
-        </div>
 
-        <div className="col-xs-6 col-md-3">
-          <h6>Quick Links</h6>
-          <ul className="footer-links">
-                <li>
-                <Link to="Home" spy={true} smooth={true} offset={0} duration={500} >
-                    Home
-                </Link>
-                </li>
-                <li>
-                <Link to="About" spy={true} smooth={true} offset={0} duration={500} >
-                    About
-                </Link>
-                </li>
-                <li>
-                <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}>
-                    Projects
-                </Link>
-                </li>
-                <li>
-                <Link to="Contact" spy={true} smooth={true} offset={0} duration={500}>
-                    Contact
-                </Link>
-                </li>
-          </ul>
-        </div>
-      </div>
-      <hr/>
-    </div>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 col-sm-6 col-xs-12">
-          <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by  &nbsp;
-            <a  href="http://www.nirajkumar.in">Niraj Kumar</a>
-          </p>
-        </div>
-
-        <div className="col-md-4 col-sm-6 col-xs-12">
-          <ul className="social-icons">
-            <li>
-                <a href="https://github.com/im-niraj/" target="_blank">
-                    <i class="fa fa-github" aria-hidden="true"></i>
-                </a>
-                </li>
-                <li>
-                <a href="https://www.linkedin.com/in/imniraj/" target="_blank">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
-                </li>
-                <li>
-                <a href="https://www.instagram.com/n1r4jkumar/" target="_blank">
-                    <i className='fa fa-instagram'></i>
-                </a>
-                </li>
-                <li>
-                <a href="https://www.youtube.com/channel/UCMj15TEUdhEjr2YHL-qO7uQ" target="_blank">
-                    <i className='fa fa-youtube-play'></i>
-                </a>
-                </li>
-                <li>
-                <a href="https://twitter.com/n1r4jkumar" target="_blank">
-                    <i className='fa fa-twitter'></i>
-                </a>  
-                </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+            <div className="container-footer-text">
+                <p>©{date}. Design with {<ImHeart color='red'/>} by Niraj.</p>
+            </div>
     </footer>
   )
 }
